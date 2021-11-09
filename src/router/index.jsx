357b75c11210b0107be85f576e842a199d82710b
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-11-09 18:27:00 +0800

    feat(page title)

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,50 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ReactDocumentTitle from 'react-document-title'
 
 import Home from '@/modules/HomeDemo/pages/home.jsx'
 import Demo from '@/modules/TestDemo/pages/test.jsx'
 
+const routerMap = [
+  {
+    path: '/',
+    name: 'home',
+    meta: {
+      title: 'Home'
+    },
+    component: Home,
+  },
+  {
+    path: '/demo',
+    name: 'demo',
+    meta: {
+      title: 'Demo'
+    },
+    component: Demo,
+  }
+]
+
+// const after
+
 const router = () => (
-      // {/* <Route exact path="/demo" component={Demo}/> */}
   <Router>
     <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/test" component={Demo}/>
+      {
+        routerMap.map((routerItem, idx) => (
+          <Route
+            exact
+            key={idx}
+            path={routerItem.path}
+            render={
+              props => (
+                <ReactDocumentTitle title={routerItem.meta.title}>
+                  <routerItem.component />
+                </ReactDocumentTitle>
+              )
+            }
+          />
+        ))
+      }
     </Switch>
   </Router>
 )
