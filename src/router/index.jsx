b413752ef41db7e0c7bf33d60cf6cc6aec6ed4d3
GGupzHH <1248023357@qq.com>
2021-11-13 14:52:27 +0800

    feat(lazy components)

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,85 +4,11 @@ import ReactDocumentTitle from 'react-document-title'
 
 import PrivateRoute from './privateRoute'
 
-import Home from '@/modules/HomeDemo/pages/home.jsx'
-import Home1 from '@/modules/HomeDemo/pages/home1.jsx'
-import Demo from '@/modules/TestDemo/pages/test.jsx'
-import TestDemo1 from '@/modules/TestDemo/pages/test1.jsx'
-import DemoParams from '@/modules/TestDemo/pages/test-params'
-
-import NotFound from '@/components/404'
-
-
-const routerMap = [
-
-  {
-    path: '/demo',
-    name: 'demo',
-    meta: {
-      title: 'Demo'
-    },
-    component: Demo,
-  },
-]
-
 const RouteNotFound = () => {
   console.log('🍉🍉🍉🍉🍉🍉🍉')
   return <Redirect to={Object.assign({}, location, { state: { notFoundError: true } }) }></Redirect>
 }
 
-// const Routeeee = React.lazy(() => {
-
-//   return (
-//   <Route render={(props) => {
-//     console.log(props)
-//     const notFoundError = props.location.state?.notFoundError
-//     return (
-//       notFoundError ?
-//       <NotFound/> :
-//       <Switch>
-//         <Route path={'/test'} render={
-//           props => (
-//             <Demo {...props}>
-//               <Switch>
-//                 <Route exact path="/test/a">
-//                   <Home {...props}></Home>
-//                 </Route>
-//                 <Route exact path="/test/b">
-//                   <Home1 {...props}></Home1>
-//                 </Route>
-//                 <RouteNotFound/>
-//               </Switch>
-//               {/* <Route path="*" component={NotFound}/> */}
-//             </Demo>
-//           )
-//         }>
-//         </Route>
-//         <Route path={'/testparams'} render={
-//           props => {
-//             return (
-//               <TestDemo1>
-//                 <Switch>
-//                   <Route exact path='/testparams/home'>
-//                   <DemoParams/>
-//                   </Route>
-//                   <Route exact path='/testparams/:id'>
-//                     <DemoParams/>
-//                     123
-//                   </Route>
-//                 </Switch>
-//               </TestDemo1>
-//             )
-//           }
-//         }>
-//         </Route>
-//         <Route path="*" component={NotFound}/>
-//       </Switch>
-//     )
-//   }
-// }>
-//   </Route>)
-// }
-// )
 function sleep (time = 0) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -91,52 +17,23 @@ function sleep (time = 0) {
   })
 }
 
-const Foo = React.lazy(async () => {
+const LazyComponent = React.lazy(async () => {
+  // 异步操作 验证token 跳转
   await sleep(1000)
-  console.log(3333)
 
-  // return import('@/modules/HomeDemo/pages/home.jsx')
-  return import('@/modules/HomeDemo/pages/home.jsx')
+  if (false) {
+    return import('@/components/404')
+  } else {
+    return import('./privateRoute')
+  }
 })
 
 const router = () => (
   <Router>
-      {/* <Routeeee/> */}
-      <Route exact path='/testparams/home' >
-          <React.Suspense fallback={<div>loading...</div>}>
-              <Foo/>
-          </React.Suspense>
-      </Route>
+    <React.Suspense fallback={<div>loading...</div>}>
+      <LazyComponent/>
+    </React.Suspense>
   </Router>
 )
-
-// <Route
-          //   exact
-          //   key={index}
-          //   path={routerItem.path}
-          //   render={
-          //     props => {
-          //       console.log(routerItem)
-          //       return (
-          //         <ReactDocumentTitle title={routerItem.meta.title}>
-          //           <routerItem.component />
-          //         </ReactDocumentTitle>
-          //       )
-          //     }
-          //   }
-          // />
-// {<Route
-//   exact
-//   key={idx}
-//   path={routerItem.path}
-//   render={
-//     props => (
-//       <ReactDocumentTitle title={routerItem.meta.title}>
-//         <routerItem.component />
-//       </ReactDocumentTitle>
-//     )
-//   }
-// />}
-
 
 export default router
--- src/router/privateRoute.jsx
+++ src/router/privateRoute.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 
 import { Switch, Route, Redirect, useLocation } from 'react-router-dom'
 
-// class PrivateRoute extends React.Component {
-//   constructor(props) {
-//     super(props)
-//   }
-//   render () {
-//     console.log(this.props)
-//     return (
-//       <Route exact path={this.props.path} component={this.props.component} />
-//     )
-//   }
-// }
+import Home from '@/modules/HomeDemo/pages/home.jsx'
+import Home1 from '@/modules/HomeDemo/pages/home1.jsx'
+import Demo from '@/modules/TestDemo/pages/test.jsx'
+import TestDemo1 from '@/modules/TestDemo/pages/test1.jsx'
+import DemoParams from '@/modules/TestDemo/pages/test-params'
+
+import NotFound from '@/components/404'
+
+const routerMap = [
+
+  {
+    path: '/demo',
+    name: 'demo',
+    meta: {
+      title: 'Demo'
+    },
+    component: Demo,
+  },
+]
+
+const RouteNotFound = () => {
+  console.log('🍉🍉🍉🍉🍉🍉🍉')
+  return <Redirect to={Object.assign({}, location, { state: { notFoundError: true } }) }></Redirect>
+}
 
 const PrivateRoute = function ({
   component: Component,
@@ -22,38 +35,56 @@ const PrivateRoute = function ({
   console.log(Component)
   console.log(childrenRouters)
   console.log(rest)
-  // return (
-  //   <Route
-  //     exact
-  //     path={ rest.path }
-  //     render={ props => {
-  //       return (
-  //         <Component />
-  //       )
-  //     }}
-  //   >
-  //   </Route>
-  // )
+
   return (
-    <Route
-      exact={true}
-      path={rest.path}
-      render={
-        props => {
+    <Route render={(props) => {
+          console.log(props)
+          const notFoundError = props.location.state?.notFoundError
           return (
-            <Component {...props} {...rest}>
-              {Array.isArray(childrenRouters) ? (
-                <Switch>
-                  {childrenRouters.map((routerItem, index) => {
-                    return <PrivateRoute { ...routerItem } key={ index }/>
-                  })}
-                </Switch>
-              ) : null}
-            </Component>
+            notFoundError ?
+            <NotFound/> :
+            <Switch>
+              <Route path={'/test'} render={
+                props => (
+                  <Demo {...props}>
+                    <Switch>
+                      <Route exact path="/test/a">
+                        <Home {...props}></Home>
+                      </Route>
+                      <Route exact path="/test/b">
+                        <Home1 {...props}></Home1>
+                      </Route>
+                      <RouteNotFound/>
+                    </Switch>
+                    {/* <Route path="*" component={NotFound}/> */}
+                  </Demo>
+                )
+              }>
+              </Route>
+              <Route path={'/testparams'} render={
+                props => {
+                  return (
+                    <TestDemo1>
+                      <Switch>
+                        <Route exact path='/testparams/home'>
+                          <DemoParams/>
+                        </Route>
+                        <Route exact path='/testparams/:id'>
+                          <DemoParams/>
+                          123
+                        </Route>
+                      </Switch>
+                    </TestDemo1>
+                  )
+                }
+              }>
+              </Route>
+              <Route path="*" component={NotFound}/>
+            </Switch>
           )
         }
-      }
-    />
+      }>
+        </Route>
   )
 }
 
