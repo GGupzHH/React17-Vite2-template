3b34b237dc3bbded8acde362a3c35d1c6caed175
赵雅国 <zhao.yaguo@shinewingdigital.com>
2021-11-15 15:55:50 +0800

    feat(recursive router)

--- src/router/privateRoute.jsx
+++ src/router/privateRoute.jsx
@@ -5,39 +5,42 @@ import { Switch, Route, Redirect, useLocation } from 'react-router-dom'
 import ReactDocumentTitle from 'react-document-title'
 
 import App from '@/App'
-import Home from '@/modules/HomeDemo/pages/home.jsx'
-import Home1 from '@/modules/HomeDemo/pages/home1.jsx'
-import Demo from '@/modules/TestDemo/pages/test.jsx'
-import TestDemo1 from '@/modules/TestDemo/pages/test1.jsx'
-import DemoParams from '@/modules/TestDemo/pages/test-params'
 
 import NotFound from '@/components/404'
 
-const routerMap = [
-
-  {
-    path: '/demo',
-    name: 'demo',
-    meta: {
-      title: 'Demo'
-    },
-    component: Demo,
-  },
-]
+import routerMap from '@/router/routerMap.js'
 
 const RouteNotFound = () => {
   console.log('🍉🍉🍉🍉🍉🍉🍉')
   return <Redirect to={Object.assign({}, location, { state: { notFoundError: true } }) }></Redirect>
 }
 
+const recursiveRouters = routeList => (
+  Array.isArray(routeList) &&
+  routeList.map((routeItem, index) => (
+    <Route path={routeItem.path} key={index} exact={routeItem.exact} render={
+      props => (
+        <ReactDocumentTitle title={routeItem.meta.title}>
+          <routeItem.component>
+            <Switch>
+              {
+                recursiveRouters(routeItem.child)
+              }
+              <RouteNotFound />
+            </Switch>
+          </routeItem.component>
+        </ReactDocumentTitle>
+      )
+    }>
+    </Route>
+  ))
+)
+
 const PrivateRoute = function ({
   component: Component,
   childrenRouters,
   ...rest
 }) {
-  console.log(Component)
-  console.log(childrenRouters)
-  console.log(rest)
 
   return (
     <Route render={(props) => {
@@ -51,48 +54,9 @@ const PrivateRoute = function ({
                   <NotFound/>
                 </ReactDocumentTitle> :
                 <Switch>
-                  <Route path={'/test'} render={
-                    props => (
-                      <Demo {...props}>
-                        <Switch>
-                          <Route exact path="/test/a">
-                            <ReactDocumentTitle title='home'>
-                              <Home {...props}></Home>
-                            </ReactDocumentTitle>
-                          </Route>
-                          <Route exact path="/test/b">
-                            <ReactDocumentTitle title='home1'>
-                              <Home1 {...props}></Home1>
-                            </ReactDocumentTitle>
-                          </Route>
-                          <RouteNotFound/>
-                        </Switch>
-                        {/* <Route path="*" component={NotFound}/> */}
-                      </Demo>
-                    )
-                  }>
-                  </Route>
-                  <Route path={'/testparams'} render={
-                    props => {
-                      return (
-                        <TestDemo1>
-                          <Switch>
-                            <Route exact path='/testparams/home'>
-                              <ReactDocumentTitle title='demoParams'>
-                                <DemoParams/>
-                              </ReactDocumentTitle>
-                            </Route>
-                            <Route exact path='/testparams/:id'>
-                              <ReactDocumentTitle title='demoParams ID'>
-                                <DemoParams/>
-                              </ReactDocumentTitle>
-                            </Route>
-                          </Switch>
-                        </TestDemo1>
-                      )
-                    }
-                  }>
-                  </Route>
+                  {
+                    recursiveRouters(routerMap)
+                  }
                   <Route path="*" component={NotFound}/>
                 </Switch>
               }
@@ -100,7 +64,7 @@ const PrivateRoute = function ({
           )
         }
       }>
-        </Route>
+    </Route>
   )
 }
 
---
+++ src/router/routerMap.js
@@ -0,0 +1,77 @@
+import Home from '@/modules/HomeDemo/pages/home.jsx'
+import Home1 from '@/modules/HomeDemo/pages/home1.jsx'
+import Demo from '@/modules/TestDemo/pages/test.jsx'
+import TestDemo1 from '@/modules/TestDemo/pages/test1.jsx'
+import DemoParams from '@/modules/TestDemo/pages/test-params'
+
+const routerMap = [
+  {
+    path: '/test',
+    name: 'demo',
+    meta: {
+      title: 'test'
+    },
+    component: Demo,
+    child: [
+      {
+        path: '/test/a',
+        name: 'demo',
+        meta: {
+          title: 'test/a'
+        },
+        component: Home,
+        // exact: true,
+        child: [
+          {
+            path: '/test/a/c',
+            name: 'demo',
+            meta: {
+              title: 'est/a/c'
+            },
+            component: DemoParams,
+            exact: true,
+          },
+        ]
+      },
+      {
+        path: '/test/b',
+        name: 'demo',
+        meta: {
+          title: 'Demo'
+        },
+        component: Home1,
+        exact: true,
+      }
+    ]
+  },
+  {
+    path: '/testparams',
+    name: 'testparams',
+    meta: {
+      title: 'testparams'
+    },
+    component: TestDemo1,
+    child: [
+      {
+        path: '/testparams/home',
+        name: 'demo',
+        meta: {
+          title: 'Demo'
+        },
+        component: DemoParams,
+        exact: true,
+      },
+      {
+        path: '/testparams/:id',
+        name: 'demo',
+        meta: {
+          title: 'Demo'
+        },
+        component: DemoParams,
+        exact: true,
+      }
+    ]
+  }
+]
+
+export default routerMap
